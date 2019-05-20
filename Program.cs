bf6f9cef3a29526596da276c65707088ac6d231d
Jatinder Singh <Jatinder.Singh@atcltd.atc.org.nz>
2019-05-21 10:57:23 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FindVowel
+{
+    // This program is used to find that vowel.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Enter any Char ");
+            char ch = char.Parse(Console.ReadLine());
+            String st = ch.ToString().ToLower();
+            switch (st)
+            {
+                case "a":
+                    Console.WriteLine(st + " is a vowel");
+                    break;
+                case "e":
+                    Console.WriteLine(st + " is a vowel");
+                    break;
+                case "i":
+                    Console.WriteLine(st + " is a vowel");
+                    break;
+                case "o":
+                    Console.WriteLine(st + " is a vowel");
+                    break;
+                case "u":
+                    Console.WriteLine(st + " is a vowel");
+                    break;
+                default:
+                    Console.WriteLine(st + " is not a vowel");
+                    break;
+
+            }
+        }
+    }
+}
